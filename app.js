09f0c128face1aab3994bb56f2e24fef0095f553
edvvarrd <edvvardsky@gmail.com>
2023-12-28 20:39:00 +0200

    proxy repair

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const path = require('path')
 const cors = require('cors')
 
 require('express-async-errors')
